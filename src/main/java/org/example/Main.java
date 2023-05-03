9fef8d0fdf11ff1a4ae54d34603761fe56ac17bb
dialaay5 <diala.ay5@gmail.com>
2023-05-03 03:10:42 +0300

    upload all

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Question 1: ");
+        System.out.println("מכיוון שכל המשתנים המוגדרים הם אובייקטים, אז הפוינטר שלהם נשמר בסטאק והערך עצמו נשמר בהיפ (בערימה) והפוינטר מצביע על הכתובת שלהם בערימה");
+
+        System.out.println("Question 2: ");
+        System.out.println("300.0\n" + "5000.0\n" + "5000.0\n" + "5000.0");
+    }
+}
